df09cf83397b3f9228f0c2677d91cc30f273f1f0
GanHoL <GanHoL@163.com>
2019-06-08 14:33:25 +0800

    firstcommit

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,5 +2,21 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("testgit");
+
+        System.out.println("git love");
+
+        System.out.println("gitgitgit");
+
+        System.out.println("开始");
+
+        System.out.println("结束");
+
+        System.out.println("!!!!!");
+
+        System.out.println("????");
+
+        System.out.println("java php");
+
+
     }
 }
